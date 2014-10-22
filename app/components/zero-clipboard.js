43baf8de4a9d4829065b4609028b4e17998af3aa
Chris Manson <chris@manson.ie>
2014-10-22 16:20:27 +0100

    adding hover and active class

--- app/components/zero-clipboard.js
+++ app/components/zero-clipboard.js
@@ -5,6 +5,12 @@ export default Ember.Component.extend({
 	attributeBindings: ['title', 'data-clipboard-text'],
 	title: 'Copy to clipboard',
 	didInsertElement: function () {
+	  // see https://gist.github.com/Mic92/5232331
+	  ZeroClipboard.config({
+		hoverClass: "hover",
+		activeClass: "active"
+	  });
+
 	  var client = new ZeroClipboard(this.get('element'));
 
 	  //bind aftercopy to an ember event
