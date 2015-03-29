b032fd7348d0faeeda7df0a120717278d2d5acd0
Ditofry <brandon@ibotta.com>
2015-03-28 23:46:11 -0600

    syntax and function error cleanup

--- addon/components/zero-clipboard.js
+++ addon/components/zero-clipboard.js
@@ -4,26 +4,17 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	attributeBindings: ['title', 'data-clipboard-text', 'data-clipboard-target'],
 	title: 'Copy to clipboard',
+	zeroClipboardEvents: ['ready', 'beforeCopy', 'copy', 'afterCopy', 'destroy'],
 	didInsertElement: function () {
 	  var client = new ZeroClipboard(this.get('element'));
-
-		var events = [
-			'ready',
-			'beforeCopy',
-			'copy',
-			'afterCopy',
-			'destroy'
-		] // 'error'
+		// 'error'
 		var _this = this;
-		events.each(function(action){
+		//bind Zero Clipboard events to ember events
+		this.get('zeroClipboardEvents').forEach(function(action){
 			client.on(action, Ember.run.bind(this, function(event) {
 				this.send(action, event);
 		  }));
 		}, _this);
-		//bind aftercopy to an ember event
-	  // client.on("aftercopy", Ember.run.bind(this, function(event) {
-		// 	this.send('afterCopy', event);
-	  // }));
 	},
 	"data-clipboard-text": function(){
 		return this.get('text');
