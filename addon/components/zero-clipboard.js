4eade16440a7b75f054b37422c0d3c18d2631073
Ben Limmer <ben@benlimmer.com>
2015-06-14 20:05:26 -0600

    Ember CLI 0.2.7.

--- addon/components/zero-clipboard.js
+++ addon/components/zero-clipboard.js
@@ -2,21 +2,23 @@ import Ember from 'ember';
 /* global ZeroClipboard */
 
 export default Ember.Component.extend({
-	attributeBindings: ['title', 'data-clipboard-text', 'data-clipboard-target'],
-	title: 'Copy to clipboard',
-	didInsertElement: function () {
-	  var client = new ZeroClipboard(this.get('element'));
+  attributeBindings: ['title', 'data-clipboard-text', 'data-clipboard-target'],
+  title: 'Copy to clipboard',
+  
+  didInsertElement: function() {
+    var client = new ZeroClipboard(this.get('element'));
 
-		//bind aftercopy to an ember event
-	  client.on("aftercopy", Ember.run.bind(this, function(event) {
-			this.send('afterCopy', event);
-	  }));
-	},
-	"data-clipboard-text": function(){
-		return this.get('text');
-	}.property('text'),
+    //bind aftercopy to an ember event
+    client.on("aftercopy", Ember.run.bind(this, function(event) {
+      this.send('afterCopy', event);
+    }));
+  },
 
-	"data-clipboard-target": function(){
-		return this.get('cbTarget');
-	}.property('cbTarget')
+  "data-clipboard-text": Ember.computed('text', function() {
+    return this.get('text');
+  }),
+
+  "data-clipboard-target": Ember.computed('cbTarget', function() {
+    return this.get('cbTarget');
+  })
 });
